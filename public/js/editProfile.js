2e725e8e82311ca2e65cfd3c18c5151ed555aa9b
AxelAlba <54636454+AxelAlba@users.noreply.github.com>
2020-03-29 18:02:57 +0800

    Fixed editProfile Issue.

--- public/js/editProfile.js
+++ public/js/editProfile.js
@@ -12,36 +12,13 @@ $(document).ready(() => {
     $('#update').click(() => {
         //get form data
         var profilePic = $('#uploadPreview').attr('src');
-
-        var firstName;
-        if ($('#firstName').val() == null) firstName = $('#firstName').attr('placeholder');
-        else firstName = $('#firstName').val();
-
-        var lastName;
-        if ($('#lastName').val() == null) lastName = $('#lastName').attr('placeholder');
-        else lastName = $('#lastName').val();
-
-        var biography;
-        if ($('#biography').val() == null) biography = $('#biography').attr('placeholder');
-        else biography = $('#biography').val();
-
-        var email;
-        if ($('#email').val() == null) email = $('#email').attr('placeholder');
-        else email = $('#email').val();
-
-        var mobileNum;
-        if ($('#mobileNum').val() == null) mobileNum = $('#mobileNum').attr('placeholder');
-        else mobileNum = $('#mobileNum').val();
-
-        var telNum;
-        if ($('#telNum').val() == null) telNum = $('#telNum').attr('placeholder');
-        else telNum = $('#telNum').val();
-
-        var address;
-        if ($('#address').val() == null) address = $('#address').attr('placeholder');
-        else address = $('#address').val();
-
-
+        var firstName = $('#firstName').val();
+        var lastName = $('#lastName').val();
+        var biography = $('#biography').val();
+        var email = $('#email').val();
+        var mobileNum = $('#mobileNum').val();
+        var telNum = $('#telNum').val();
+        var address = $('#address').val();
         var profile = {
             //uid 
             email: email,
