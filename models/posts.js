37e44ea0b16d480b61450b8fc000131e804c4928
danjohnvelasco <54620209+danjohnvelasco@users.noreply.github.com>
2020-04-03 16:30:22 +0800

    styled view-post

--- models/posts.js
+++ models/posts.js
@@ -15,25 +15,14 @@ const PostSchema = new mongoose.Schema(
   }
 );
 
-/*
-  Note:
-    Only the functions of this model should be exported, not the actual model. All the database related methods should
-    only be in the models.
-
-  example: 
-  exports.query = function(pattern, sort, next) {
-    studentModel.find({ name: { $regex: pattern } }).sort(sort).exec( function(err, students){
-        next(students); // next() == *function callback at the controller*
-    });
-  };
-*/
 const PostModel = mongoose.model('Post', PostSchema);
 
-//REFACTOR THIS LATER
+// Get all Posts
 exports.getDiscoverPosts = function (next) {
-  PostModel.find({}, 'img')
+  PostModel.find({}, 'img') // Add filter for not followed
     .exec((err, data) => {
       if (err) throw err;
+
       var posts = [];
       data.forEach((post) => {
         posts.push(post.toObject());
